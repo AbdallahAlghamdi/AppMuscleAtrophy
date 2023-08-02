d98df5454255a83e4393269589e3b33824401ce8
AbdallahAlghamdi <75899918+AbdallahAlghamdi@users.noreply.github.com>
2023-08-02 22:17:25 +0300

    Fixing domain

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/WholesomePaws')
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
