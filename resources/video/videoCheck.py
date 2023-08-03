1772a74ec84a2132c8957f6b7bde09b2d4389785
AbdallahAlghamdi <75899918+AbdallahAlghamdi@users.noreply.github.com>
2023-08-03 16:56:50 +0300

    testing digitalOcean

--- resources/video/videoCheck.py
+++ resources/video/videoCheck.py
@@ -5,7 +5,7 @@ from flask_restful import Resource, Api, reqparse
 class VideoCheck(Resource):
     def get(self, video_id):
         mycursor = getConnection()
-        mycursor.execute('select * from videos where ID = ', video_id )
+        mycursor.execute('select * from videos where ID=', video_id )
         result = mycursor.fetchall()
         
         return result,222
